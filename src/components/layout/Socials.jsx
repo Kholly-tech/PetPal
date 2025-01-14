12e8fc9d4e3ff7523a7ceb10f8eb4cdb2cf8c78e
Kolawole Akintayo <81145173+Kholly-tech@users.noreply.github.com>
2025-01-14 17:47:14 +0100

    Update Socials.jsx 3

--- src/components/layout/Socials.jsx
+++ src/components/layout/Socials.jsx
@@ -25,7 +25,7 @@ const Socials = () => {
               <Icon icon="simple-icons:gmail" width="24" height="24" />
           </a>
         </div>
-        <div className="text-center md:absolute md:left-1/2 md:transform md:-translate-x-1/2">
+        <div className="text-center md:absolute md:left-1/2 md:transform md:-translate-x-1/2 md:ml-4">
           <p class name='md:ml-6'>Developed by KOL-CONCEPTS © {currentYear} - All Rights Reserved @ <a href='/'>PetPal</a></p>
         </div>
       </div>
